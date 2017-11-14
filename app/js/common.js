9ae286e69a8f0385431dd04cf748c61d88f27be2
Lara <lara.chechh@gmail.com>
2017-11-14 21:55:36 +0200

    tab on space

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,23 @@
 $(function() {
 
-	$('#my-menu').mmenu({
-		extensions: [ 'theme-black', 'effect-menu-slide', 'pagedim-black' ],
-		navbar: {
-			title: '<img src="img/logo-1.png" alt="Logo">'
-		},
-		offCanvas: {
-			position: 'right'
-		}
-	});
-	
-	var api = $('#my-menu').data('mmenu');
-	api.bind("open:start",function() {
-		$('.hamburger').addClass('is-active');
-	});
-	api.bind("close:before",function(){
-		$('.hamburger').removeClass('is-active');
-	});
-	 // api.bind( "openPanel:start", function( $panel ) {
+    $('#my-menu').mmenu({
+        extensions: [ 'theme-black', 'effect-menu-slide', 'pagedim-black' ],
+        navbar: {
+            title: '<img src="img/logo-1.png" alt="Logo">'
+        },
+        offCanvas: {
+            position: 'right'
+        }
+    });
+    
+    var api = $('#my-menu').data('mmenu');
+    api.bind("open:start",function() {
+        $('.hamburger').addClass('is-active');
+    });
+    api.bind("close:before",function(){
+        $('.hamburger').removeClass('is-active');
+    });
+     // api.bind( "openPanel:start", function( $panel ) {
   //        console.log( "This panel is now opening: #" + $panel.attr( "id" ) );
   //     });
   //     api.bind( "openPanel:finish", function( $panel ) {
